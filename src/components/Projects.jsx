a470d687091390fdf108065761afa76d5644e1ad
mohitbakshi00 <mohit19bakshi@gmail.com>
2023-10-15 14:14:45 +0530

    Refactored projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
-import booking from "../assets/booking.png";
 import courseWebsite from "../assets/courseWebsite.png"
 import foodOrderingApp from "../assets/foodOrderingApp.png"
 import technologyTrends from "../assets/technologyTrends.png"
-
 
 const Projects = () => {
 
@@ -25,7 +23,7 @@ const Projects = () => {
       src: technologyTrends,
       title: "Technology Trends Analysis in IT Sector",
       link: "https://github.com/mohitbakshi00/technology-usage-trends-analysis",
-    },
+    }
   ]
 
   return (
@@ -37,11 +35,8 @@ const Projects = () => {
             key={id}
             className={`sm:w-[600px] h-[450px] mx-auto my-6 shadow-lg duration-500 py-2 rounded-lg bg-gray-900 `}>
             <p className="my-2 text-center text-2xl text-white py-1 font-bold">{title}</p>
-
             <img src={src} alt="" className="max-h-[300px]  md:max-h-[300px] w-full px-6 object-cover rounded-xl border-x-black" />
-          
               <a href={link} target="_blank" rel="noreferrer" ><button className="text-white bg-gradient-to-b from-pink-500 to-red-500 px-6 py-2 my-4 mx-auto  flex justify-between items-center rounded-md hover:scale-110 duration-300">View</button></a>
-           
           </div>
         ))}
       </div>
